58acd95e4d50aaa8d2c3b76bef5a9c9c13cfaa2a
Billy Huang <10646003@ntub.edu.tw>
2022-02-01 21:53:28 +0800

    feat(HeaderCartButton): Using the Context

--- src/components/layout/HeaderCartButton.tsx
+++ src/components/layout/HeaderCartButton.tsx
@@ -1,17 +1,27 @@
+import { useContext } from 'react';
+
 import CartIcon from '@icons/CartIcon';
+import CartContext from '@store/cartContext';
 
 type HeaderCartButtonProps = {
   onClick: () => void;
 };
 
 const HeaderCartButton = ({ onClick }: HeaderCartButtonProps) => {
+  const cartContext = useContext(CartContext);
+
+  const numberOfCartItems = cartContext.items.reduce(
+    (total, item) => total + item.amount,
+    0,
+  );
+
   return (
     <button className='button' onClick={onClick}>
       <span className='icon'>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className='badge'>3</span>
+      <span className='badge'>{numberOfCartItems}</span>
     </button>
   );
 };
--- src/store/cartContext.ts
+++ src/store/cartContext.ts
@@ -3,7 +3,7 @@ import React from 'react';
 import { CartItem } from '@models/cartItem';
 
 const CartContext = React.createContext({
-  items: [],
+  items: [] as CartItem[],
   totalAmount: 0,
   addItem: (item: CartItem) => {},
   removeItem: (id: string) => {},
