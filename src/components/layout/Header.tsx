ee002349e428cf5a1db19470c88ff06846ae431f
Billy Huang <10646003@ntub.edu.tw>
2022-02-01 18:15:48 +0800

    feat(HeaderCartButton): Adding the "Cart" Button Component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,12 @@
 import mealsImage from '@images/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   return (
     <>
       <header className='header'>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className='main-image'>
         <img src={mealsImage} alt='A table full of delicious food!' />
