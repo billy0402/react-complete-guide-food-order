1c9589a12269a566b80e8648375ce85a9952832c
Billy Huang <10646003@ntub.edu.tw>
2022-02-03 21:58:52 +0800

    feat(AvailableMeals): Handling the Loading State

--- src/components/meal/AvailableMeals.tsx
+++ src/components/meal/AvailableMeals.tsx
@@ -6,16 +6,27 @@ import MealItem from './meal-item/MealItem';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState<Meal[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMeals = async () => {
+    setIsLoading(true);
     const response = await fetch('http://localhost:3001/meals');
     const responseData = await response.json();
     setMeals(responseData);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className='meals__loading'>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   return (
     <section className='meals'>
