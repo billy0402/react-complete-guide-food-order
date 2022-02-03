ee92cad92c669d94f5a9f4e0dda658b0311b80b7
Billy Huang <10646003@ntub.edu.tw>
2022-02-03 21:53:47 +0800

    feat(AvailableMeals): Fetching Meals via Http

--- src/components/meal/AvailableMeals.tsx
+++ src/components/meal/AvailableMeals.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
+
 import Card from '@components/ui/Card';
-import { DUMMY_MEALS } from '@fixtures/meals';
+import { Meal } from '@models/meal';
 import MealItem from './meal-item/MealItem';
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const fetchMeals = async () => {
+    const response = await fetch('http://localhost:3001/meals');
+    const responseData = await response.json();
+    setMeals(responseData);
+  };
+
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
   return (
     <section className='meals'>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((meal) => (
+          {meals.map((meal) => (
             <MealItem key={meal.id} meal={meal} />
           ))}
         </ul>
