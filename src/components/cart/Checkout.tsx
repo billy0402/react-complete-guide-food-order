abebe13000f817237c2315f5280023440b27d6a5
Billy Huang <10646003@ntub.edu.tw>
2022-02-03 23:29:10 +0800

    feat(Checkout): Adding Form Validation

--- src/components/cart/Checkout.tsx
+++ src/components/cart/Checkout.tsx
@@ -1,10 +1,23 @@
-import { SyntheticEvent, useRef } from 'react';
+import { SyntheticEvent, useRef, useState } from 'react';
+
+const isEmpty = (value: string) => value.trim() === '';
+const isLimitChars = (value: string, limitLength: number) =>
+  value.trim().length === limitLength;
 
 type CheckoutProps = {
   onCancel: () => void;
 };
 
 const Checkout = ({ onCancel }: CheckoutProps) => {
+  const [formInputValidity, setFormInputValidity] = useState<{
+    [key: string]: boolean;
+  }>({
+    name: false,
+    street: false,
+    postalCode: false,
+    city: false,
+  });
+
   const nameInputRef = useRef<HTMLInputElement>(null);
   const streetInputRef = useRef<HTMLInputElement>(null);
   const postalCodeInputRef = useRef<HTMLInputElement>(null);
@@ -13,29 +26,67 @@ const Checkout = ({ onCancel }: CheckoutProps) => {
   const confirmHandler = (event: SyntheticEvent) => {
     event.preventDefault();
 
-    const enteredName = nameInputRef.current?.value;
-    const enteredStreet = streetInputRef.current?.value;
-    const enteredPostalCode = postalCodeInputRef.current?.value;
-    const enteredCity = cityInputRef.current?.value;
+    if (
+      !nameInputRef.current ||
+      !streetInputRef.current ||
+      !postalCodeInputRef.current ||
+      !cityInputRef.current
+    ) {
+      throw Error('inputRef is not assigned');
+    }
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const isNameValid = !isEmpty(enteredName);
+    const isStreetValid = !isEmpty(enteredStreet);
+    const isPostalCodeValid =
+      !isEmpty(enteredPostalCode) && isLimitChars(enteredPostalCode, 5);
+    const isCityValid = !isEmpty(enteredCity);
+    setFormInputValidity({
+      name: isNameValid,
+      street: isStreetValid,
+      postalCode: isPostalCodeValid,
+      city: isCityValid,
+    });
+
+    const isFormValid =
+      isNameValid && isStreetValid && isPostalCodeValid && isCityValid;
+    if (!isFormValid) {
+      return;
+    }
+
+    // Submit the cart data
   };
+
+  const invalidControlClass = (field: string) =>
+    formInputValidity[field] ? '' : ' checkout__control--invalid';
 
   return (
     <form className='checkout__form' onSubmit={confirmHandler}>
-      <div className='checkout__control'>
+      <div className={`checkout__control${invalidControlClass('name')}`}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' />
+        {!formInputValidity.name && <p>Please input a valid name!</p>}
       </div>
-      <div className='checkout__control'>
+      <div className={`checkout__control${invalidControlClass('street')}`}>
         <label htmlFor='street'>Street</label>
         <input ref={streetInputRef} type='text' id='name' />
+        {!formInputValidity.street && <p>Please input a valid street!</p>}
       </div>
-      <div className='checkout__control'>
+      <div className={`checkout__control${invalidControlClass('postalCode')}`}>
         <label htmlFor='postal'>Postal code</label>
         <input ref={postalCodeInputRef} type='text' id='postal' />
+        {!formInputValidity.postalCode && (
+          <p>Please input a valid postal code (5 characters long)!</p>
+        )}
       </div>
-      <div className='checkout__control'>
+      <div className={`checkout__control${invalidControlClass('city')}`}>
         <label htmlFor='city'>City</label>
         <input ref={cityInputRef} type='text' id='city' />
+        {!formInputValidity.city && <p>Please input a valid city!</p>}
       </div>
 
       <div className='checkout__actions'>
